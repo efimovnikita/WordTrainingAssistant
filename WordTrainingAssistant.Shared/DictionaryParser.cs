2db68118a78dfa56f57c49bbcc1db89b94de113f
maskedball <maskedballmail@gmail.com>
2022-06-29 16:07:22 +0300

    Refactoring

--- WordTrainingAssistant.Shared/DictionaryParser.cs
+++ WordTrainingAssistant.Shared/DictionaryParser.cs
@@ -7,10 +7,10 @@ namespace WordTrainingAssistant.Shared
     public static class DictionaryParser
     {
         private static Parser<KeyValuePair<string, string>> DictionaryRow =>
-            from word in Parse.CharExcept(':').Many().Text()
+            from name in Parse.CharExcept(':').Many().Text()
             from separator in Parse.Char(':')
             from translation in Parse.AnyChar.Except(Parse.LineTerminator).Many().Text()
-            select new KeyValuePair<string, string>(word, translation);
+            select new KeyValuePair<string, string>(name, translation);
 
         private static Parser<KeyValuePair<string, string>[]> FullDictionary =>
             from rows in DictionaryRow.DelimitedBy(Parse.LineTerminator)
